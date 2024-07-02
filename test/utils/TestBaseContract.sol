30c39080eece950fa080cb139893b08e3e27f943
Ramesh Nair <ram@hiddentao.com>
2024-07-02 17:41:10 +0100

    test: test skeleton base

--- test/utils/TestBaseContract.sol
+++ test/utils/TestBaseContract.sol
@@ -2,11 +2,20 @@
 pragma solidity >=0.8.21;
 
 import "forge-std/Test.sol";
+
+import { ERC20 } from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import { IDiamondCut } from "lib/diamond-2-hardhat/contracts/interfaces/IDiamondCut.sol";
 import { DiamondProxy } from "src/generated/DiamondProxy.sol";
 import { IDiamondProxy } from "src/generated/IDiamondProxy.sol";
 import { LibDiamondHelper } from "src/generated/LibDiamondHelper.sol";
 import { InitDiamond } from "src/init/InitDiamond.sol";
+
+contract MockERC20 is ERC20 {
+  constructor() ERC20("MockERC20", "MOCKERC20") {}
+  function mint(address account, uint256 value) external {
+    _mint(account, value);
+  }  
+}
 
 abstract contract TestBaseContract is Test {
   address public immutable account0 = address(this);
@@ -14,6 +23,7 @@ abstract contract TestBaseContract is Test {
   address public account2;
 
   IDiamondProxy public diamond;
+  MockERC20 public tribalToken;
 
   function setUp() public virtual {
     console2.log("\n -- Test Base\n");
@@ -30,9 +40,12 @@ abstract contract TestBaseContract is Test {
     console2.log("Deploy diamond");
     diamond = IDiamondProxy(address(new DiamondProxy(account0)));
 
+    console2.log("Deploy Mock ERC20");
+    tribalToken = new MockERC20();
+
     console2.log("Cut and init");
     IDiamondCut.FacetCut[] memory cut = LibDiamondHelper.deployFacetsAndGetCuts(address(diamond));
     InitDiamond init = new InitDiamond();
-    diamond.diamondCut(cut, address(init), abi.encodeWithSelector(init.init.selector));
+    diamond.diamondCut(cut, address(init), abi.encodeWithSelector(init.init.selector, address(tribalToken), account0));
   }
 }
