74f64e489ff0bbe06814e33a5c7fdb6427476bcd
Ramesh Nair <ram@hiddentao.com>
2024-07-30 20:26:13 +0100

    fix: credshield-bug-4 - pragma fixes

--- src/facets/GatewayFacet.sol
+++ src/facets/GatewayFacet.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0
-pragma solidity ^0.8.24;
+pragma solidity 0.8.24;
 
 import { AppStorage, LibAppStorage } from "src/libs/LibAppStorage.sol";
 import { LibErrors } from "src/libs/LibErrors.sol";
