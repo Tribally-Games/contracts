75a975d8566be98c44450084f8111f100a06df7e
Ramesh Nair <ram@hiddentao.com>
2024-07-30 20:25:00 +0100

    fix: credshield-bug-3 - safe transfer

--- src/libs/LibTribalToken.sol
+++ src/libs/LibTribalToken.sol
@@ -2,15 +2,20 @@
 pragma solidity ^0.8.24;
 
 import { IERC20 } from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import { SafeERC20 } from "lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 import { LibAppStorage } from "src/libs/LibAppStorage.sol";
 
 
 library LibTribalToken {
     function transferFrom(address _from, uint256 _amount) internal {
-        IERC20(LibAppStorage.diamondStorage().tribalToken).transferFrom(_from, address(this), _amount);
+        IERC20 token = IERC20(LibAppStorage.diamondStorage().tribalToken);
+
+        SafeERC20.safeTransferFrom(token, _from, address(this), _amount);
     }
 
     function transferTo(address _to, uint256 _amount) internal {
-        IERC20(LibAppStorage.diamondStorage().tribalToken).transfer(_to, _amount);
+        IERC20 token = IERC20(LibAppStorage.diamondStorage().tribalToken);
+
+        SafeERC20.safeTransfer(token, _to, _amount);
     }
 }
