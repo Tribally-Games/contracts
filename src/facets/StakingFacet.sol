fc79e76800baaeaa9d5c4fccb786af1df31ad865
Ramesh Nair <ram@hiddentao.com>
2024-09-21 14:19:19 +0800

    refactor: prefix all with staking word to signify usage

--- src/facets/StakingFacet.sol
+++ src/facets/StakingFacet.sol
@@ -7,12 +7,37 @@ import { LibErrors } from "src/libs/LibErrors.sol";
 import { LibAuth } from "../libs/LibAuth.sol";
 
 contract StakingFacet {
-    event Deposited(address user, uint256 amount);
-    event Withdrawn(address user, uint256 amount);
-    event Claimed(address user, address claimToken, uint256 amount);
-    event PayoutRecorded(address token, uint256 amount, uint256 day);
+    /** @dev Emitted when a user deposits stake
+     * @param user Address of the user who deposited
+     * @param amount Amount of tokens deposited
+     */
+    event StakeDeposited(address user, uint256 amount);
 
-    function depositStake(address user, uint256 amount) external {
+    /** @dev Emitted when a user withdraws stake
+     * @param user Address of the user who withdrew
+     * @param amount Amount of tokens withdrawn
+     */
+    event StakeWithdrawn(address user, uint256 amount);
+
+    /** @dev Emitted when a user claims payouts
+     * @param user Address of the user who claimed
+     * @param claimToken Address of the token claimed
+     * @param amount Amount of tokens claimed
+     */
+    event StakePayoutClaimed(address user, address claimToken, uint256 amount);
+
+    /** @dev Emitted when a payout is recorded
+     * @param token Address of the token paid out
+     * @param amount Amount of tokens paid out
+     * @param day The day the payout was recorded (timestamp / 1 days)
+     */
+    event StakePayoutRecorded(address token, uint256 amount, uint256 day);
+
+    /** @dev Deposits stake for a user
+     * @param user Address of the user depositing stake
+     * @param amount Amount of tokens to deposit
+     */
+    function stakeDeposit(address user, uint256 amount) external {
         if (amount == 0) {
           revert LibErrors.AmountMustBeGreaterThanZero();
         }
@@ -27,10 +52,14 @@ contract StakingFacet {
         s.stakingUserTotalStaked[user] += amount;
         s.stakingTotalStaked += amount;
 
-        emit Deposited(user, amount);
+        emit StakeDeposited(user, amount);
     }
 
-    function withdrawStake(address user, uint256 amount) external {
+    /** @dev Withdraws stake for a user
+     * @param user Address of the user withdrawing stake
+     * @param amount Amount of tokens to withdraw
+     */
+    function stakeWithdraw(address user, uint256 amount) external {
         if (amount == 0) {
           revert LibErrors.AmountMustBeGreaterThanZero();
         }
@@ -49,17 +78,22 @@ contract StakingFacet {
         s.stakingUserTotalStaked[user] -= amount;
         s.stakingTotalStaked -= amount;
 
-        emit Withdrawn(user, amount);
+        emit StakeWithdrawn(user, amount);
     }
 
-    function claimPayouts(uint256 amount, address claimToken, address user, AuthSignature calldata signature) external {
+    /** @dev Allows a user to claim staking payouts
+     * @param amount Amount of tokens to claim
+     * @param claimToken Address of the token to claim
+     * @param user Address of the user claiming
+     * @param signature AuthSignature for verification
+     */
+    function stakeClaimPayouts(uint256 amount, address claimToken, address user, AuthSignature calldata signature) external {
         AppStorage storage s = LibAppStorage.diamondStorage();
         
         if (amount == 0) {
           revert LibErrors.AmountMustBeGreaterThanZero();
         }
         
-        // Use LibAuth.assertValidSignature instead of verifySignature
         LibAuth.assertValidSignature(
             user,
             s.signer,
@@ -75,10 +109,14 @@ contract StakingFacet {
         s.stakingUserLastClaimTime[user][claimToken] = block.timestamp;
         s.stakingTotalClaimed[claimToken] += amount;
 
-        emit Claimed(user, claimToken, amount);
+        emit StakePayoutClaimed(user, claimToken, amount);
     }
 
-    function recordPayout(address token, uint256 amount) external {
+    /** @dev Records a staking payout for a specific token
+     * @param token Address of the token being paid out
+     * @param amount Amount of tokens being paid out
+     */
+    function stakeRecordPayout(address token, uint256 amount) external {
         if (amount == 0) {
           revert LibErrors.AmountMustBeGreaterThanZero();
         }
@@ -100,50 +138,90 @@ contract StakingFacet {
             s.stakingPayoutPool[token].push(Transaction(block.timestamp, amount));
         }
 
-        emit PayoutRecorded(token, amount, currentDay);
+        emit StakePayoutRecorded(token, amount, currentDay);
     }
 
-    function getUserTotalStaked(address user) external view returns (uint256) {
+    /** @dev Gets the total amount staked by a user
+     * @param user Address of the user
+     * @return Total amount staked by the user
+     */
+    function stakeGetUserTotalStaked(address user) external view returns (uint256) {
         AppStorage storage s = LibAppStorage.diamondStorage();
         return s.stakingUserTotalStaked[user];
     }
 
-    function getUserDepositCount(address user) external view returns (uint256) {
+    /** @dev Gets the number of stake deposits made by a user
+     * @param user Address of the user
+     * @return Number of deposits
+     */
+    function stakeGetUserDepositCount(address user) external view returns (uint256) {
         AppStorage storage s = LibAppStorage.diamondStorage();
         return s.stakingUserDeposits[user].length;
     }
 
-    function getUserWithdrawalCount(address user) external view returns (uint256) {
+    /** @dev Gets the number of stake withdrawals made by a user
+     * @param user Address of the user
+     * @return Number of withdrawals
+     */
+    function stakeGetUserWithdrawalCount(address user) external view returns (uint256) {
         AppStorage storage s = LibAppStorage.diamondStorage();
         return s.stakingUserWithdrawals[user].length;
     }
 
-    function getUserClaimCount(address user, address token) external view returns (uint256) {
+    /** @dev Gets the number of stake claims made by a user for a specific token
+     * @param user Address of the user
+     * @param token Address of the token
+     * @return Number of claims
+     */
+    function stakeGetUserClaimCount(address user, address token) external view returns (uint256) {
         AppStorage storage s = LibAppStorage.diamondStorage();
         return s.stakingUserClaims[user][token].length;
     }
 
-    function getUserDeposits(address user) external view returns (Transaction[] memory) {
+    /** @dev Gets all stake deposits made by a user
+     * @param user Address of the user
+     * @return Array of Transaction structs representing deposits
+     */
+    function stakeGetUserDeposits(address user) external view returns (Transaction[] memory) {
         AppStorage storage s = LibAppStorage.diamondStorage();
         return s.stakingUserDeposits[user];
     }
 
-    function getUserWithdrawals(address user) external view returns (Transaction[] memory) {
+    /** @dev Gets all stake withdrawals made by a user
+     * @param user Address of the user
+     * @return Array of Transaction structs representing withdrawals
+     */
+    function stakeGetUserWithdrawals(address user) external view returns (Transaction[] memory) {
         AppStorage storage s = LibAppStorage.diamondStorage();
         return s.stakingUserWithdrawals[user];
     }
 
-    function getUserClaims(address user, address token) external view returns (Transaction[] memory) {
+    /** @dev Gets all stake claims made by a user for a specific token
+     * @param user Address of the user
+     * @param token Address of the token
+     * @return Array of Transaction structs representing claims
+     */
+    function stakeGetUserClaims(address user, address token) external view returns (Transaction[] memory) {
         AppStorage storage s = LibAppStorage.diamondStorage();
         return s.stakingUserClaims[user][token];
     }
 
-    function getLastClaimTime(address user, address token) external view returns (uint256) {
+    /** @dev Gets the timestamp of the last stake claim made by a user for a specific token
+     * @param user Address of the user
+     * @param token Address of the token
+     * @return Timestamp of the last claim
+     */
+    function stakeGetLastClaimTime(address user, address token) external view returns (uint256) {
         AppStorage storage s = LibAppStorage.diamondStorage();
         return s.stakingUserLastClaimTime[user][token];
     }
 
-    function getDailyPayoutPoolAmount(address token, uint256 day) external view returns (uint256) {
+    /** @dev Gets the stake payout pool amount for a specific token on a given day
+     * @param token Address of the token
+     * @param day The day to query (timestamp / 1 days)
+     * @return Amount in the payout pool for the specified day
+     */
+    function stakeGetDailyPayoutPoolAmount(address token, uint256 day) external view returns (uint256) {
         AppStorage storage s = LibAppStorage.diamondStorage();
         uint256 payoutPoolLength = s.stakingPayoutPool[token].length;
         for (uint256 i = 0; i < payoutPoolLength; i++) {
@@ -154,7 +232,11 @@ contract StakingFacet {
         return 0;
     }
 
-    function getTotalClaimedForToken(address token) external view returns (uint256) {
+    /** @dev Gets the total amount claimed for a specific staking token
+     * @param token Address of the token
+     * @return Total amount claimed
+     */
+    function stakeGetTotalClaimedForToken(address token) external view returns (uint256) {
         AppStorage storage s = LibAppStorage.diamondStorage();
         return s.stakingTotalClaimed[token];
     }
